55a42634ec8324fe3cf803c9eb12d7ccc40bde82
RenLevingar <110576801+RenLevingar@users.noreply.github.com>
2023-11-21 15:22:56 -0700

    New

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import List from './componets/List'
+import data from './utils/data'
+import { useState } from 'react'
+
+function App(){
+  const [people, setPeople] = useState(data);
+  const removePeople = (id) => {
+    let newPeople = people.filter((person) => person.id !== id)
+    setPeople(newPeople)
+  }
+
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} remove={removePeople}/>
+        <div className='buttons'>
+          <button onClick={()=> setPeople([])}>Clear</button>
+          <button onClick={()=> setPeople(data)}>Reset</button>
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default App;
